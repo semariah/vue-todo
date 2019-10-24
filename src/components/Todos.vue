b2e026a6d041842970f7860fa47d977f9f64741d
sk <semaria.eyzac@gmail.com>
2019-10-24 12:48:03 -0700

    pass the data from App.vue to the Todos.vue as a prop by attaching it to the v-bind in parent component and using the key word props in child component and have it displayed by using the v-for directive since its an array in the Todos.vue component

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <h1>ToDo</h1>
+    <div v-for="todo in todos"></div>
+    <h1>{{todo.title}}<h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Todos"
+  name: "Todos",
+  props: ["todos"]
 };
 </script>
 
 <style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+.is-complete {
+  text-decoration: line-through;
+}
 </style>
