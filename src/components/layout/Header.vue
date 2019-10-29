19d5dda4ff053ce6cf2a048b33887bf6e70e82d1
sk <semaria.eyzac@gmail.com>
2019-10-28 20:49:24 -0700

    finally done and pushing to github before adding the router

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="header">
     <h1>ToDo</h1>
   </header>
 </template>
@@ -9,4 +9,15 @@ export default {
 };
 </script>
 <style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+.header a {
+  color: #fff;
+  padding-right: 5px;
+  text-decoration: none;
+}
 </style>
